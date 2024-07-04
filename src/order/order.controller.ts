04de8ea6505f571bfc294dad5f237c29a15da349
Francisco.I <francisco.insaurralde21@gmail.com>
2024-07-04 06:43:40 -0300

    feature: now oder controller have all the endpoints required

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,20 +1,26 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('order')
 export class OrderController {
-    /*
-    Get /order
-    GET /order/:id
-    POST /order
-    PATCH /order/:id
-    DELETE /order/:id 
-    */
+
    @Get() //Get /order
    findAll(){
     return []
    }
    @Get(":id") //GET /order/:id
    findOne(@Param("id") id:string){
+    return{id}
+   }
+   @Post() //POST /order
+   createOrder(@Body() user:{}){
+    return user
+   }
+   @Patch(":id") //PATCH /order/:id
+   editOrder(@Param("id") id:string, @Body() orderUpdate: {}){
+        return {id, ...orderUpdate}
+   }
+   @Delete(":id") //DELETE /order/:id 
+   deleteOrder(@Param("id") id:string){
     return{id}
    }
 }
