e1036efb5bb953b8f2b5e25c73ab52369bd1db1e
Francisco.I <francisco.insaurralde21@gmail.com>
2024-07-09 11:45:36 -0300

    feature: order controller and service linked

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,26 +1,50 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { OrderService } from './order.service';
+
+type AddItem = {
+   name: string;
+   value: boolean | number;
+   price: number;
+};
+
+
+type Order = {
+   id: number;
+   name: string;
+   image: string;
+   description: string;
+   visible: boolean;
+   add?: AddItem[];
+};
 
 @Controller('order')
 export class OrderController {
 
+   constructor (private readonly OrderService: OrderService) {}
+
    @Get() //Get /order
-   findAll(){
-    return []
+   findAll(@Query("role") role?: "USER"| "ADMIN"){
+    return this.OrderService.findAll(role)
    }
+
    @Get(":id") //GET /order/:id
    findOne(@Param("id") id:string){
-    return{id}
+      return this.OrderService.findOne(+id)
    }
+
    @Post() //POST /order
-   createOrder(@Body() user:{}){
-    return user
+   createOrder(@Body() order:Order){
+      return this.OrderService.createOrder(order)
    }
-   @Patch(":id") //PATCH /order/:id
-   editOrder(@Param("id") id:string, @Body() orderUpdate: {}){
-        return {id, ...orderUpdate}
+
+   @Patch(":id") //PATCH /order/:id NO ANDA
+   editOrder(@Body() editedOrder: Order){ //ESTO SE TIENE QUE CAMBIAR APRA QUE RECIBA EL ID Y LA COMIDA CAMBIADA SEA LA DEFINIDA POR EL ID
+        return this.OrderService.editOrder(editedOrder)
    }
+
    @Delete(":id") //DELETE /order/:id 
-   deleteOrder(@Param("id") id:string){
-    return{id}
+   deleteOrder(@Param("id") id:number){ //no anda
+    return this.OrderService.deleteOrder(id)
    }
+
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,44 +3,80 @@ import { Injectable } from '@nestjs/common';
 
 type AddItem = {
     name: string;
-    value: boolean | number; // It seems like value can be either a boolean or a number
+    value: boolean | number;
     price: number;
 };
 
-// Define the type for the order
+
 type Order = {
     id: number;
     name: string;
     image: string;
     description: string;
     visible: boolean;
-    add: AddItem[];
+    add?: AddItem[];
 };
 
 @Injectable()
 export class OrderService {
     
-    private orders= [
+    private orders: Order[] = [
         {
-              id: 1,
-              name: "cheese burger",
-              image:"https://www.mostachos.com.ar/adm/files/articulos/1712868317.jpg",
-              description: "Includes filing, moisturizing, and permanent polish application.",
-              visible: true,
-              add: [{name:"Medallon Extra", value:false, price:700},{name:"Porcion de Papas Extra", value:3, price:700},{name:"Panceta Extra", value:false, price:700}]
+            id: 1,
+            name: "cheese burger",
+            image:"https://www.mostachos.com.ar/adm/files/articulos/1712868317.jpg",
+            description: "Includes filing, moisturizing, and permanent polish application.",
+            visible: true,
+            add: [{name:"Medallon Extra", value:false, price:700},{name:"Porcion de Papas Extra", value:3, price:700},{name:"Panceta Extra", value:false, price:700}]
         }
     ]
-    findAll(role?: "USER"| "ADMIN"){
+
+    findAll(role?: "USER"| "ADMIN"){//if is USER only have to get the visible ones
         if (role === "ADMIN"){
-            return "PESUTI"
+            return "ADMIN Request"
         }
         return this.orders
     }
+
     findOne(id:number){
         const order = this.orders.find(order => order.id === id)
         return order
     }
-    createOrder(order: Order) {
 
+    createOrder(order: Order, role?: "USER" | "ADMIN") {
+        const highestId = this.orders.reduce((max, order) => order.id > max ? order.id : max, 0);
+
+        const newOrder: Order = {
+            id: highestId + 1,
+            name: order.name,
+            image: order.image,
+            description: order.description,
+            visible: order.visible,
+            add: order.add || []
+            
+        };
+
+        this.orders.push(newOrder);
+
+        return this.orders
     }
+
+    editOrder(editedOrder: Order, role?: "USER" | "ADMIN"){
+        this.orders = this.orders.map(order =>{
+            if (order.id === editedOrder.id){
+                return {...order, ...editedOrder}
+            }
+            return order
+        })
+        return this.findOne(editedOrder.id)
+    }
+
+    deleteOrder(id:number, role?: "USER" | "ADMIN"){
+        const removedOrder = this.findOne(id)
+
+        this.orders= this.orders.filter(order => order.id !== id)
+
+        return removedOrder
+    }
+
 }
