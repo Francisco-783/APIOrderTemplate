b244ca1ba4dde76269d6d472bbaa5b8df47c3cb2
Francisco.I <francisco.insaurralde21@gmail.com>
2024-07-18 18:39:07 -0300

    feature: create database component

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [OrderModule],
+  imports: [OrderModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
