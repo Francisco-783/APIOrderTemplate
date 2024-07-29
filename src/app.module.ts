437a96499ec91a92205e6d36c82fad1254cc45bd
Francisco.I <francisco.insaurralde21@gmail.com>
2024-07-29 14:55:26 -0300

    feature: Throttler applied

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
 import { DatabaseModule } from './database/database.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [OrderModule, DatabaseModule],
+  imports: [OrderModule, DatabaseModule, ThrottlerModule.forRoot([{
+    ttl: 60000,
+    limit: 3,
+  }])],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
 })
 export class AppModule {}
