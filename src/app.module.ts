2e6567764b2ec693255c2ca6a0babd4ff1de8074
Francisco.I <francisco.insaurralde21@gmail.com>
2024-08-30 20:22:51 -0300

    feature: admin component created

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,13 +11,14 @@ import { MyLoggerModule } from './my-logger/my-logger.module';
 import { ExtraModule } from './extra/extra.module';
 import { AddItemModule } from './addItem/addItem.module';
 import { ClientRequestModule } from './client-request/client-request.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [OrderModule, DatabaseModule, ThrottlerModule.forRoot([{
     name: "long",
     ttl: 60000,
     limit: 50
-  }]), PromoModule, MyLoggerModule, ExtraModule, AddItemModule, ClientRequestModule],
+  }]), PromoModule, MyLoggerModule, ExtraModule, AddItemModule, ClientRequestModule, AdminModule],
   controllers: [AppController, PromoController],
   providers: [AppService, {
     provide: APP_GUARD,
