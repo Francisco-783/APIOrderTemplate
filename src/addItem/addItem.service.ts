817f82885d19100755f0eef248a44b4af0649282
Francisco.I <francisco.insaurralde21@gmail.com>
2024-09-06 19:41:31 -0300

    feature: catch changed to return the error to front

--- src/addItem/addItem.service.ts
+++ src/addItem/addItem.service.ts
@@ -1,38 +1,87 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAddItemDto } from '../dto/addItem/create-addItem.dto';
 import { UpdateAddItemDto } from '../dto/addItem/update-addItem.dto';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class AddItemService {
-  constructor(private prisma: DatabaseService) {}
+  constructor(private readonly databaseModule: DatabaseService) {}
 
   async create(createAdditemDto: CreateAddItemDto) {
-    return await this.prisma.addItem.create({
-      data: createAdditemDto,
-    });
+    try {
+      return await this.databaseModule.addItem.create({
+        data: createAdditemDto,
+      });
+    } catch (error) {
+      console.error('Error while creating AddItem:', error);
+      throw error; // Lanza el error para que sea manejado por NestJS
+    }
   }
 
   async findAll() {
-    return await this.prisma.addItem.findMany();
+    try {
+      return await this.databaseModule.addItem.findMany();
+    } catch (error) {
+      console.error('Error while getting all AddItems:', error);
+      throw error; // Lanza el error para que sea manejado por NestJS
+    }
   }
 
   async findOne(id: string) {
-    return await this.prisma.addItem.findUnique({
-      where: { id },
-    });
+    try {
+      const addItem = await this.databaseModule.addItem.findUnique({
+        where: { id },
+      });
+
+      if (!addItem) {
+        throw new NotFoundException('El aditivo que buscas no existe');
+      }
+
+      return addItem;
+    } catch (error) {
+      console.error('Error while getting AddItem:', error);
+      throw error; 
+    }
   }
 
   async update(id: string, updateAdditemDto: UpdateAddItemDto) {
-    return await this.prisma.addItem.update({
-      where: { id },
-      data: updateAdditemDto,
+    try {
+
+      const addItemExists = await this.databaseModule.addItem.findUnique({
+        where: { id },
     });
+
+    if (!addItemExists) {
+      throw new NotFoundException('El aditivo que quieres editar no existe');
+  }
+
+      return await this.databaseModule.addItem.update({
+        where: { id },
+        data: updateAdditemDto,
+      });
+    } catch (error) {
+      console.error('Error while updating AddItem:', error);
+      throw error;
+    }
   }
 
   async remove(id: string) {
-    return await this.prisma.addItem.delete({
-      where: { id },
+    try {
+
+      const addItemExists = await this.databaseModule.addItem.findUnique({
+        where: { id },
     });
+
+    if (!addItemExists) {
+      throw new NotFoundException('El aditivo que quieres eliminar no existe');
+  }
+
+      return await this.databaseModule.addItem.delete({
+        where: { id },
+      });
+    } catch (error) {
+      console.error('Error while deleting AddItem:', error);
+      throw error;
+    }
   }
 }
--- src/client-request/client-request.service.ts
+++ src/client-request/client-request.service.ts
@@ -1,44 +1,84 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateClientRequestDto } from '../dto/client-request/create-client-request.dto';
 import { UpdateClientRequestDto } from '../dto/client-request/update-client-request.dto';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class ClientRequestService {
-  constructor(private databaseModule: DatabaseService) {}
+  constructor(private readonly databaseModule: DatabaseService) {}
 
   async createClientRequest(data: CreateClientRequestDto) {
-    return await this.databaseModule.clientRequest.create({
-      data: {
-        status: data.status,
-        promos: {
-          connect: data.promos?.map(promo => ({ id: promo.id })),
+    try {
+      return await this.databaseModule.clientRequest.create({
+        data: {
+          status: data.status,
+          promos: {
+            connect: data.promos?.map(promo => ({ id: promo.id })),
+          },
+          orders: {
+            connect: data.orders?.map(order => ({ id: order.id })),
+          },
+          extras: {
+            connect: data.extras?.map(extra => ({ id: extra.id })),
+          },
         },
-        orders: {
-          connect: data.orders?.map(order => ({ id: order.id })),
-        },
-        extras: {
-          connect: data.extras?.map(extra => ({ id: extra.id })),
-        },
-      }
-    });
+      });
+    } catch (error) {
+      console.error('Error while creating Client Request:', error);
+      throw error; 
+    }
   }
+
+
 
   async findAllClientRequest() {
-    return await this.databaseModule.clientRequest.findMany();
+    try {
+      return await this.databaseModule.clientRequest.findMany();
+    } catch (error) {
+      console.error('Error while getting all Client Requests:', error);
+      throw error; 
+    }
   }
+
 
   async findOneClientRequest(id: string) {
-    return await this.databaseModule.clientRequest.findUnique({
-      where: { id },
-    });
+    try {
+      const clientRequest = await this.databaseModule.clientRequest.findUnique({
+        where: { id },
+      });
+
+      if (!clientRequest) {
+        throw new NotFoundException('El Client Request que buscas no existe');
+      }
+
+      return clientRequest;
+    } catch (error) {
+      console.error('Error while getting a Client Request:', error);
+      throw error; 
+    }
   }
 
+
   async updateClientRequest(id: string, updateClientRequestDto: UpdateClientRequestDto) {
-    return await this.databaseModule.clientRequest.update({
-      where: { id },
-      data: updateClientRequestDto,
+    try {
+
+      const requestExists = await this.databaseModule.clientRequest.findUnique({
+        where: { id },
     });
+
+    if (!requestExists) {
+      throw new NotFoundException('el pedido que quieres editar no existe');
   }
+
+      return await this.databaseModule.clientRequest.update({
+        where: { id },
+        data: updateClientRequestDto,
+      });
+    } catch (error) {
+      console.error('Error while updating Client Request:', error);
+      throw error; 
+    }
+  }
+
 
 }
