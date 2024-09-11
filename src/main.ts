15939477537715ee235a27cbe66a0cc48d6df0bc
Francisco.I <francisco.insaurralde21@gmail.com>
2024-09-11 17:25:36 -0300

    feature: now the API runs in https

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MyLoggerService } from './my-logger/my-logger.service';
+import * as fs from 'fs';
+
+
+
 
 async function bootstrap() { //this start the project
+
+  const httpsOptions = {
+    key: fs.readFileSync( __dirname + '../../server.key'),
+    cert: fs.readFileSync(__dirname + '../../server.cert'),
+  };
+
   const app = await NestFactory.create(AppModule,
-    {bufferLogs:true}
+    {bufferLogs:true, httpsOptions}
   );
   app.useLogger(app.get(MyLoggerService))
   app.enableCors()
