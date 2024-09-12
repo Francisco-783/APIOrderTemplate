85af1ff315dd5c46986919d9ac80a8ab7869717c
Francisco.I <francisco.insaurralde21@gmail.com>
2024-09-12 19:01:53 -0300

    feature: create authentication component

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,13 +12,14 @@ import { ExtraModule } from './extra/extra.module';
 import { AddItemModule } from './addItem/addItem.module';
 import { ClientRequestModule } from './client-request/client-request.module';
 import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [OrderModule, DatabaseModule, ThrottlerModule.forRoot([{
     name: "long",
     ttl: 60000,
     limit: 50
-  }]), PromoModule, MyLoggerModule, ExtraModule, AddItemModule, ClientRequestModule, AdminModule],
+  }]), PromoModule, MyLoggerModule, ExtraModule, AddItemModule, ClientRequestModule, AdminModule, AuthModule],
   controllers: [AppController, PromoController],
   providers: [AppService, {
     provide: APP_GUARD,
