475d79522be88149bbc0ab62fa3786e552f54d1c
Francisco.I <francisco.insaurralde21@gmail.com>
2024-09-12 19:53:37 -0300

    making authentication of admin, after this the backend is done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AdminService } from 'src/admin/admin.service'; 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private adminService: AdminService) {}
+
+  async signIn(name: string, pass: string): Promise<any> {
+    const admin = await this.adminService.findOneAdmin(name);
+    if (admin?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const { password, ...result } = admin;
+    // TODO: Generate a JWT and return it here
+    // instead of the user object
+    return result;
+  }
+}
