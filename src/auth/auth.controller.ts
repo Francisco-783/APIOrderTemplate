5864c705ea33bd8c2c5ae6caa8042b2ea3682d7a
Francisco.I <francisco.insaurralde21@gmail.com>
2024-09-13 15:56:32 -0300

    feature: auth controller completed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginAdminDto } from 'src/dto/admin/login-admin.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: LoginAdminDto) {
+    return this.authService.signIn(signInDto.name, signInDto.password);
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { AdminModule } from 'src/admin/admin.module';
 
 @Module({
+  imports: [AdminModule],
+  providers: [AuthService],
   controllers: [AuthController],
-  providers: [AuthService]
 })
 export class AuthModule {}
