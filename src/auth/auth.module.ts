9b65ae7ccba50fd03ade095d37abfecd4a0cb926
Francisco.I <francisco.insaurralde21@gmail.com>
2024-09-17 23:57:37 -0300

    feature: auth module completed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AdminModule } from 'src/admin/admin.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AdminModule],
+  imports: [
+    AdminModule,
+    ConfigModule.forRoot(), // Habilitar el uso de variables de entorno
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '60s' },
+      }),
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
