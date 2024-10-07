52df0c9716971a91f64edce5259ce32e587262e3
Francisco.I <francisco.insaurralde21@gmail.com>
2024-10-07 19:12:43 -0300

    feature: import Modules for usage

--- src/client-request/client-request.module.ts
+++ src/client-request/client-request.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ClientRequestService } from './client-request.service';
 import { ClientRequestController } from './client-request.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { OrderModule } from 'src/order/order.module';
+import { ExtraModule } from 'src/extra/extra.module';
+import { PromoModule } from 'src/promo/promo.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, OrderModule, ExtraModule, PromoModule],
   controllers: [ClientRequestController],
   providers: [ClientRequestService],
 })
