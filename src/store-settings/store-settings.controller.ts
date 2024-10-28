f5af3e4b661c41f332b0ea6fc6624f365b6a366d
Francisco.I <francisco.insaurralde21@gmail.com>
2024-10-28 10:34:55 -0300

    feature: getOneStatus of store-settings completed

--- src/store-settings/store-settings.controller.ts
+++ src/store-settings/store-settings.controller.ts
@@ -1,11 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { StoreSettingsService } from './store-settings.service';
 
 @Controller('store-settings')
 export class StoreSettingsController {
-  constructor(private readonly storeSettingsService: StoreSettingsService) {
+  constructor(private readonly storeSettingsService: StoreSettingsService) {}
 
-
-    
+  @Get(":id")
+  getOneStatus(@Param("id") id:string) {
+    return this.storeSettingsService.isStoreOpen(id);
   }
 }
