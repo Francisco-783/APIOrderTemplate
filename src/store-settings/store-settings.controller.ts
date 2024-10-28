1e4273189617e60261fd759fb6de6ae83faa227d
Francisco.I <francisco.insaurralde21@gmail.com>
2024-10-28 11:37:46 -0300

    feature: getallstatus and updateStatus is completed

--- src/store-settings/store-settings.controller.ts
+++ src/store-settings/store-settings.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
 import { StoreSettingsService } from './store-settings.service';
 
 @Controller('store-settings')
@@ -9,4 +9,15 @@ export class StoreSettingsController {
   getOneStatus(@Param("id") id:string) {
     return this.storeSettingsService.isStoreOpen(id);
   }
+
+  @Get()
+  getAllStatus(){
+    return this.storeSettingsService.getAllStoreStatus();
+  }
+
+  @Patch(":id")
+  async updateStatus(@Body('isOpen') isOpen: boolean, @Param("id") id:string) {
+    return this.storeSettingsService.setIsOpen(isOpen, id);
+  }
 }
+
