2a6f9438ac04188b9edb811e08ff03a015e80a1d
Francisco.I <francisco.insaurralde21@gmail.com>
2024-10-29 11:24:33 -0300

    feature. store-settings controller working

--- src/store-settings/store-settings.controller.ts
+++ src/store-settings/store-settings.controller.ts
@@ -1,23 +1,33 @@
-import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query, ValidationPipe } from '@nestjs/common';
 import { StoreSettingsService } from './store-settings.service';
+import { CreateStoreSettings } from 'src/dto/store-settings/create-store-settings.dto';
+import { Public } from 'src/auth/auth.guard';
 
 @Controller('store-settings')
 export class StoreSettingsController {
   constructor(private readonly storeSettingsService: StoreSettingsService) {}
 
+  @Public()
   @Get(":id")
   getOneStatus(@Param("id") id:string) {
     return this.storeSettingsService.isStoreOpen(id);
   }
 
+  @Public()
   @Get()
   getAllStatus(){
     return this.storeSettingsService.getAllStoreStatus();
   }
 
+  
   @Patch(":id")
-  async updateStatus(@Body('isOpen') isOpen: boolean, @Param("id") id:string) {
+  updateStatus(@Body('isOpen') isOpen: boolean, @Param("id") id:string) {
     return this.storeSettingsService.setIsOpen(isOpen, id);
+  }
+
+  @Post()
+  createStore(@Body(ValidationPipe) store:CreateStoreSettings){
+    return this.storeSettingsService.createStore(store)
   }
 }
 
