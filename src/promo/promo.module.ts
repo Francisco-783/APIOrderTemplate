f87c254b6c461bcbc79f5b63eba88a876cd47d9c
Francisco.I <francisco.insaurralde21@gmail.com>
2024-11-06 15:12:48 -0300

    minor fixes

--- src/promo/promo.module.ts
+++ src/promo/promo.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PromoService } from './promo.service';
 import { PromoController } from './promo.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [PromoController],
   providers: [PromoService],
   exports: [PromoService],
